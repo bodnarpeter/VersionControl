9b52778f10a0d29f24d1a650efcc5424bac09db1
Peter Bodnar <bodnarpeter94@gmail.com>
2020-10-18 23:25:38 +0200

    GetExchangeRates

--- 6.gyak_BodnarPeter_ppy8uy/6.gyak_BodnarPeter_ppy8uy/Form1.cs
+++ 6.gyak_BodnarPeter_ppy8uy/6.gyak_BodnarPeter_ppy8uy/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.gyak_BodnarPeter_ppy8uy.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,19 @@ namespace _6.gyak_BodnarPeter_ppy8uy
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
+
+
         }
     }
 }
