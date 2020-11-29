867447fe6785250b3fa49de461f11e7f0a806b5e
Peter Bodnar <bodnarpeter94@gmail.com>
2020-11-29 22:41:03 +0100

    population

--- BodnarPeter_ppy8uy_10gyak/BodnarPeter_ppy8uy_10gyak/Form1.cs
+++ BodnarPeter_ppy8uy_10gyak/BodnarPeter_ppy8uy_10gyak/Form1.cs
@@ -15,12 +15,20 @@ namespace BodnarPeter_ppy8uy_10gyak
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-          
+            for (int i = 1; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
